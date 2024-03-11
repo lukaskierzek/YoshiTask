29ff838d58648ce757619cc4a5ee0d79e1308a57
Łukasz Kierzek <39835435+lukaskierzek@users.noreply.github.com>
2024-03-11 19:18:13 +0100

    Add connection to database and add option for json

--- YoshiTaskWarehouseLukaszKierzek.Server/Program.cs
+++ YoshiTaskWarehouseLukaszKierzek.Server/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using YoshiTaskWarehouseLukaszKierzek.Server.Data;
+
 namespace YoshiTaskWarehouseLukaszKierzek.Server
 {
     public class Program
@@ -10,6 +14,18 @@ namespace YoshiTaskWarehouseLukaszKierzek.Server
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            }
+            );
+            builder.Services.AddDbContext<WarehouseContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDatabase"))
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+            }
+            );
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
