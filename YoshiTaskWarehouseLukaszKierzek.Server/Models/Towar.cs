16f0e57960664eab3d6a8ef18c4c6100733f925c
Łukasz Kierzek <39835435+lukaskierzek@users.noreply.github.com>
2024-03-12 15:05:27 +0100

    Modify Server
    Add Ilosc, Cena and DokumentPrzyjecia to Towar.cs

--- YoshiTaskWarehouseLukaszKierzek.Server/Models/Towar.cs
+++ YoshiTaskWarehouseLukaszKierzek.Server/Models/Towar.cs
@@ -5,7 +5,9 @@
         public int Id { get; set; }
         public string Nazwa { get; set; }
         public string Kod {  get; set; }
-        public DokumentPrzyjecia DokumentPrzyjecia { get; set; }
+        public virtual DokumentPrzyjecia DokumentPrzyjecia { get; set; }
         public int DokumnetPrzyjeciaId { get; set;}
+        public int Ilosc { get; set; }
+        public decimal Cena { get; set; }
     }
 }
