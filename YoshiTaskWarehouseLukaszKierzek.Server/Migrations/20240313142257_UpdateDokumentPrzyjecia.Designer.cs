6feb8c1709299742f06c941eb96fea459d344ed1
Łukasz Kierzek <39835435+lukaskierzek@users.noreply.github.com>
2024-03-13 18:08:38 +0100

    Update Migration

--- YoshiTaskWarehouseLukaszKierzek.Server/Migrations/20240313142257_UpdateDokumentPrzyjecia.Designer.cs
+++ YoshiTaskWarehouseLukaszKierzek.Server/Migrations/20240313142257_UpdateDokumentPrzyjecia.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YoshiTaskWarehouseLukaszKierzek.Server.Data;
 
@@ -11,9 +12,11 @@ using YoshiTaskWarehouseLukaszKierzek.Server.Data;
 namespace YoshiTaskWarehouseLukaszKierzek.Server.Migrations
 {
     [DbContext(typeof(WarehouseContext))]
-    partial class WarehouseContextModelSnapshot : ModelSnapshot
+    [Migration("20240313142257_UpdateDokumentPrzyjecia")]
+    partial class UpdateDokumentPrzyjecia
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ YoshiTaskWarehouseLukaszKierzek.Server/Migrations/20240313142257_UpdateDokumentPrzyjecia.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YoshiTaskWarehouseLukaszKierzek.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDokumentPrzyjecia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Anulowany",
+                table: "dokumentPrzyjecia",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Zatwierdzony",
+                table: "dokumentPrzyjecia",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "dokumentPrzyjecia",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Anulowany", "Zatwierdzony" },
+                values: new object[] { 0, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "dokumentPrzyjecia",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Anulowany", "Zatwierdzony" },
+                values: new object[] { 0, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "dokumentPrzyjecia",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Anulowany", "Zatwierdzony" },
+                values: new object[] { 1, 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Anulowany",
+                table: "dokumentPrzyjecia");
+
+            migrationBuilder.DropColumn(
+                name: "Zatwierdzony",
+                table: "dokumentPrzyjecia");
+        }
+    }
+}
